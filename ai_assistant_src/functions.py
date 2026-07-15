f1bbafd33dabd198652c5a26509c58beab4e11b7
Youssef Samy <yosefsamy019@gmail.com>
2026-07-15 19:24:26 +0300

    fast fixes

--- ai_assistant_src/functions.py
+++ ai_assistant_src/functions.py
@@ -3,7 +3,7 @@ from typing import List
 import streamlit as st
 
 from models.models import ChatMsg
-from values.const import MSGS_KEY
+from values.const import MSGS_KEY, ERROR_MSG
 from values.sys_prompt import get_sys_prompt
 
 
@@ -11,6 +11,7 @@ def clear_msgs_buffer():
     st.session_state[MSGS_KEY] = [
         get_sys_prompt()
     ]
+    st.session_state[ERROR_MSG] = ""
 
 
 def get_active_chat_msgs() -> List[ChatMsg]:
